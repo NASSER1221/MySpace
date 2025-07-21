10707e22f1fe974ed1f67ddcc996a96697aaecf0
NASSER1221 <alnassri1221@gmail.com>
2025-07-21 09:08:32 +0400

    added practice 4

---
+++ leet4.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class leet4 {
+
+    public String longPrefix(String[] str){
+
+        Arrays.sort(str);
+        String first=str[0];
+        String last=str[str.length-1];
+        int index=0;
+
+        while (index<first.length() && index<last.length() ){
+
+            if (first.charAt(index)==last.charAt(index)){
+                index++;
+
+            }
+            else break;
+        }return first.substring(0,index);
+    } 
+}
